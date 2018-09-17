099b08e048a9579f262e33701147f5d1c6ae38bd
rbtprograms <rbt61690@gmail.com>
2018-09-17 16:00:30 -0700

    updated layout

--- src/components/lobby/PlayerDisplay.js
+++ src/components/lobby/PlayerDisplay.js
@@ -20,16 +20,23 @@ class PlayerDisplay extends PureComponent {
     return ( 
       <div>
         <section className={styles.playerDisplay}>
-          <h1>Bobby</h1>
-          <img src="https://toolnavy.com/customavatars/avatar38224_10.gif" />
+          <article className="title">
+            <h1 className="player-header">Bobby</h1>
+            <button className="edit">✎</button>
+          </article>
+          <div className="top-playerInfo">
+            <img src="https://toolnavy.com/customavatars/avatar38224_10.gif" />
+            <h3>Greeting: "Prepare to... DIE"</h3>
+          </div>
           <section className="playerInfo">
-            <p>Location: Portland</p>
-            <p>Rank: Private</p>
-            <p>Statistics:</p>
-            <ul>
-              <li>Total Wins: 3</li>
-              <li>Total Games: 11</li>
-            </ul>
+            <div>
+              <p>Location: Portland</p>
+              <p>Statistics:</p>
+              <ul>
+                <li>Total Wins: 3</li>
+                <li>Total Games: 11</li>
+              </ul>
+            </div>
           </section>
         </section>
 
