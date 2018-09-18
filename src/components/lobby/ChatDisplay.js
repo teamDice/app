086ef2aeb5ae18d78451864829042b207e89423a
rbtprograms <rbt61690@gmail.com>
2018-09-17 23:41:41 -0700

    some style changes on chat box

--- src/components/lobby/ChatDisplay.js
+++ src/components/lobby/ChatDisplay.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import styles from './ChatDisplay';
+import styles from './ChatDisplay.css';
 
 class ChatDisplay extends Component {
 
@@ -13,7 +13,7 @@ class ChatDisplay extends Component {
     return (
       <div className={styles.chat_display}>
         {chatroom.map(chat => (
-          <p key={chat.key}>{chat.name}: {chat.text}</p>
+          <p key={chat.key}><strong>{chat.name}</strong>: {chat.text}</p>
         ))}
       </div>
     );
