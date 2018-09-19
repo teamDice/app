b9ecd5dfc81acda6d5334074a81368360349cbdb
Arthur Jen <arthurljen@gmail.com>
2018-09-18 18:34:40 -0700

    changed gamestart to game load

--- src/components/game/actions.js
+++ src/components/game/actions.js
@@ -1,4 +1,4 @@
-import { GAME_START, GAME_END, CARD_PLAY, HAND_START, EMOTE_CLEAR_ALL, getGame } from './reducers';
+import { GAME_LOAD, GAME_END, CARD_PLAY, HAND_START, EMOTE_CLEAR_ALL, getGame } from './reducers';
 import { getUser } from '../auth/reducers';
 import { gamesRef, handsRef } from '../../services/firebaseRef';
 
@@ -12,7 +12,7 @@ export const startGame = gameKey => {
 
       
       dispatch({
-        type: GAME_START,
+        type: GAME_LOAD,
         payload: game
       });
       
